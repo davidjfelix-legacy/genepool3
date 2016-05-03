5f3f15ae2e2a9095fea2ea69cba6ef61293734ec
David J Felix <felix.davidj@gmail.com>
2016-05-03 16:16:31 -0400

    Create a homebrew gene recipe

---
+++ cookbooks/gene-homebrew/attributes/default.rb
@@ -0,0 +1,4 @@
+default['gene-homebrew']['username'] = 'splicer'
+default['adminuser']['username'] = node['gene-homebrew']['username']
+default['homebrew']['owner'] = node['gene-homebrew']['username']
+
--- cookbooks/gene-homebrew/recipes/default.rb
+++ cookbooks/gene-homebrew/recipes/default.rb
@@ -4,4 +4,5 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-
+include_recipe 'adminuser::default'
+include_recipe 'homebrew::default'
