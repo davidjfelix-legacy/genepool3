d92a04c2b3add38d462a2e63b57f20edfc4ffb38
David J Felix <felix.davidj@gmail.com>
2016-05-04 19:44:04 -0400

    Add dependencies

--- cookbooks/jetbrains-ide/metadata.rb
+++ cookbooks/jetbrains-ide/metadata.rb
@@ -1,7 +1,9 @@
-name 'jetbrains'
+name 'jetbrains-ide'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures jetbrains'
 long_description 'Installs/Configures jetbrains'
 version '0.1.0'
+
+depends 'gene-homebrew', '~> 0.1.0'
