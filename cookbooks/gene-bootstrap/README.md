c466da0dd5f32423dc10bb8f8381bcc0f3c40fba
David J Felix <felix.davidj@gmail.com>
2016-05-07 14:17:49 -0400

    Create gene-bootstrap skeleton
    - This is an opinionated setup for genepool

---
+++ cookbooks/gene-bootstrap/README.md
@@ -0,0 +1,4 @@
+# gene-bootstrap
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/gene-bootstrap/metadata.rb
@@ -0,0 +1,7 @@
+name 'gene-bootstrap'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures gene-bootstrap'
+long_description 'Installs/Configures gene-bootstrap'
+version '0.1.0'
