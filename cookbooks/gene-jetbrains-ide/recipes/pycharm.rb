1837a7307e16129ddba504fdea44bdf568288e8a
David J Felix <felix.davidj@gmail.com>
2016-05-09 15:04:13 -0400

    Add rubymine and pycharm recipes
    - Still need to add in some setup for java
    - Still need to add in attributes

--- cookbooks/gene-jetbrains-ide/recipes/pycharm.rb
+++ cookbooks/gene-jetbrains-ide/recipes/pycharm.rb
@@ -5,4 +5,22 @@ when 'darwin'
 when 'alpine', 'arch', 'debian', 'fedora', 'gentoo', 'rhel'
   include_recipe 'gene-opt'
   include_recipe 'gene-jetbrains-ide::opt-jetbrains'
+  include_recipe 'gene-java'
+
+  version = node['gene-jetbrains-ide']['pycharm-version']
+  install_path = "/opt/jetbrains-ide/pycharm-#{version}"
+
+  remote_file "#{Chef::Config[:file_cache_path]}/pycharm-#{version}.tar.gz" do
+    source "#{node['gene-jetbrains-ide']['base_pycharm_url']}/pycharm-#{version}.tar.gz"
+    checksum node['gene-jetbrains-ide']['pycharm-checksum']
+    mode '0755'
+  end
+
+  bash 'install-pycharm' do
+    cwd Chef::Config[:file_cache_path]
+    code <<-EOF
+      rm -rf /opt/jetbrains-ide/pycharm-#{version}
+      tar xf pycharm-#{version}.tar.gz -C /opt/jetbrains-ide
+    EOF
+  end
 end
---
+++ cookbooks/gene-jetbrains-ide/recipes/rubymine.rb
@@ -0,0 +1,26 @@
+case node['platform_family']
+when 'darwin'
+  include_recipe 'gene-homebrew'
+  cask_package 'rubymine'
+when 'alpine', 'arch', 'debian', 'fedora', 'gentoo', 'rhel'
+  include_recipe 'gene-opt'
+  include_recipe 'gene-jetbrains-ide::opt-jetbrains'
+  include_recipe 'gene-java'
+
+  version = node['gene-jetbrains-ide']['rubymine-version']
+  install_path = "/opt/jetbrains-ide/rubymine-#{version}"
+
+  remote_file "#{Chef::Config[:file_cache_path]}/rubymine-#{version}.tar.gz" do
+    source "#{node['gene-jetbrains-ide']['base_rubymine']}/rubymine-#{version}.tar.gz"
+    checksum node['gene-jetbrains-ide']['rubymine-checksum']
+    mode '0755'
+  end
+
+  bash 'install-' do
+    cwd Chef::Config[:file_cache_path]
+    code <<-EOF
+      rm -rf /opt/jetbrains-ide/rubymine-#{version}
+      tar xf rubymine-#{version}.tar.gz -C /opt/jetbrains-ide
+    EOF
+  end
+end
