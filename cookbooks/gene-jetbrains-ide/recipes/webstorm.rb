aaa6a7f39b86560ed55ccfc3477f91e83fc792d4
David J Felix <felix.davidj@gmail.com>
2016-05-11 23:45:27 -0400

    Add webstorm

--- cookbooks/gene-jetbrains-ide/recipes/webstorm.rb
+++ cookbooks/gene-jetbrains-ide/recipes/webstorm.rb
@@ -1,3 +1,26 @@
-include_recipe 'gene-homebrew::default'
+case node['platform_family']
+when 'darwin'
+  include_recipe 'gene-homebrew'
+  cask_package 'webstorm'
+when 'alpine', 'arch', 'debian', 'fedora', 'gentoo', 'rhel'
+  include_recipe 'gene-opt'
+  include_recipe 'gene-jetbrains-ide::opt-jetbrains'
+  include_recipe 'gene-java'
 
-homebrew_cask 'webstorm'
+  version = node['gene-jetbrains-ide']['webstorm-version']
+  install_path = "/opt/jetbrains-ide/webstorm-#{version}"
+
+  remote_file "#{Chef::Config[:file_cache_path]}/webstorm-#{version}.tar.gz" do
+    source "#{node['gene-jetbrains-ide']['base_webstorm_url']}/webstorm-#{version}.tar.gz"
+    checksum node['gene-jetbrains-ide']['webstorm-checksum']
+    mode '0755'
+  end
+
+  bash 'install-webstorm' do
+    cwd Chef::Config[:file_cache_path]
+    code <<-EOF
+      rm -rf /opt/jetbrains-ide/webstorm-#{version}
+      tar xf webstorm-#{version}.tar.gz -C /opt/jetbrains-ide
+    EOF
+  end
+end
