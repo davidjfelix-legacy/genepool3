9201d3bb8fba1cb67a858fef9b250ffcc3d219fe
David J. Felix <DavidJFelix@users.noreply.github.com>
2016-06-16 00:33:22 -0400

    Update pycharm.rb

--- cookbooks/gene-jetbrains-ide/recipes/pycharm.rb
+++ cookbooks/gene-jetbrains-ide/recipes/pycharm.rb
@@ -1,7 +1,7 @@
 case node['platform_family']
-when 'darwin'
+when 'mac_os_x'
   include_recipe 'gene-homebrew'
-  cask_package 'pycharm'
+  homebrew_cask 'pycharm'
 when 'alpine', 'arch', 'debian', 'fedora', 'gentoo', 'rhel'
   include_recipe 'gene-opt'
   include_recipe 'gene-jetbrains-ide::opt-jetbrains'
