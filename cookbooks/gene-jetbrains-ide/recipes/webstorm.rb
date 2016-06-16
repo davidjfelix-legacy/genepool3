52b57e3fe8e1f2b0ff9609838096337ab03fb964
David J. Felix <DavidJFelix@users.noreply.github.com>
2016-06-16 00:34:25 -0400

    Update webstorm.rb

--- cookbooks/gene-jetbrains-ide/recipes/webstorm.rb
+++ cookbooks/gene-jetbrains-ide/recipes/webstorm.rb
@@ -1,7 +1,7 @@
 case node['platform_family']
-when 'darwin'
+when 'mac_os_x'
   include_recipe 'gene-homebrew'
-  cask_package 'webstorm'
+  homebrew_cask 'webstorm'
 when 'alpine', 'arch', 'debian', 'fedora', 'gentoo', 'rhel'
   include_recipe 'gene-opt'
   include_recipe 'gene-jetbrains-ide::opt-jetbrains'
