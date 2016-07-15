423cb6c014043371151b2b6b7782d1af2d10e73d
David J. Felix <DavidJFelix@users.noreply.github.com>
2016-07-15 12:40:47 -0400

    Remove wonky header comment

--- cookbooks/gene-homebrew/recipes/default.rb
+++ cookbooks/gene-homebrew/recipes/default.rb
@@ -1,10 +1,3 @@
-#
-# Cookbook Name:: gene-homebrew
-# Recipe:: default
-#
-# Copyright (c) 2016 The Authors, All Rights Reserved.
-
 include_recipe 'adminuser::default'
 include_recipe 'homebrew::default'
 include_recipe 'homebrew::cask'
-
