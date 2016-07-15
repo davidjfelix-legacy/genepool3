9509c82ccb3fe4984602cc67edbcb1e4bd254b86
David J. Felix <DavidJFelix@users.noreply.github.com>
2016-07-15 12:41:34 -0400

    Remove osx server
    That hasn't come up since like version 10.5

--- cookbooks/gene-homebrew/metadata.rb
+++ cookbooks/gene-homebrew/metadata.rb
@@ -7,7 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
 supports 'mac_osx'
-supports 'mac_osx_server'
 
 depends 'adminuser', '~> 0.1.0'
 depends 'homebrew', '~> 2.1.0'
